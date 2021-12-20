639d8874bc987033d8543816ef150c5cea8db023
David McGlashan <5269729+davidmcglashan@users.noreply.github.com>
2021-12-20 13:38:03 +0000

    Fields payload now structured by id and easy to type names.

--- jbFields.py
+++ jbFields.py
@@ -27,24 +27,48 @@ def getf( ins ):
     # Is the response a good one? If so, dump the JSON.
     if jbResponse.handleResponse( response ):
         data = response.read()
+
+        # No data? Never mind ...
         if data is None:
             print( "Nope data" )
+        
         else:
+            fs = json.loads( data.decode("utf-8") )
+
+            # Set the storage up.
             global fields
             fields = dict()
-            fs = json.loads( data.decode("utf-8") )
+            idToPretty = dict()
+            easyToType = dict()
+            fields["idToPretty"] = idToPretty
+            fields["easyToType"] = easyToType
+
             for field in fs:
-                fields[field["id"]] = field["name"]
+                # Map the field IDs to their pretty names.
+                idToPretty[field["id"]] = field["name"]
+
+                # Map the easy to type names to the IDs
+                easyToType[ easify( field["name"] ) ] = field["id"]
 
     # Display the findings where appropriate.
     if jbEcho.level == 3:
         print( json.dumps( fields, indent=4, sort_keys=True ) )
     
     if jbEcho.level > 1:
-        print( "%s fields loaded" % len(fields) )
+        print( "%s fields loaded" % len(idToPretty) )
 
     if callback != None:
         callback( fields )
+
+# =======================================================================================
+# Converts the pretty name of a field into something easy to type.
+# =======================================================================================
+def easify( str ):
+    PERMITTED_CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ " 
+    str = str.lower()
+    str = "".join(c for c in str if c in PERMITTED_CHARS)
+    str = str.strip().replace( " ", "-" )
+    return str
 
 # =======================================================================================
 # Look up a single field's posh name. Returns the dull name if the posh one isn't found.
